071df221db2f1e5759adb5c65c2d08ac78eea3e7
Parsa Samadnejad <troddenspade@gmail.com>
2021-07-03 21:37:55 +0430

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 mog2_backsub = cv2.createBackgroundSubtractorMOG2(detectShadows=True) #60
 # backSub = cv2.bgsegm.BackgroundSubtractorGMG() #20
-# mog_backsub = cv2.bgsegm.createBackgroundSubtractorMOG()
+mog_backsub = cv2.bgsegm.createBackgroundSubtractorMOG()
 knn_backsub = cv2.createBackgroundSubtractorKNN(detectShadows=True) #80
 
 CUT_X = 70
@@ -27,26 +27,24 @@ while True:
     I_mog = mog2_backsub.apply(I)
     I_knn = knn_backsub.apply(I)
 
-    mog_rate = 0.25
+    mog_rate = 0.6
     I = ((1-mog_rate)*I_knn + mog_rate*I_mog).astype(np.uint8)
 
     _, I = cv2.threshold(I, 250, 255, cv2.THRESH_BINARY)
-    # _, I_mog = cv2.threshold(I_mog, 240, 255, cv2.THRESH_BINARY)
-    # _, I_knn = cv2.threshold(I_knn, 240, 255, cv2.THRESH_BINARY)
 
-    n_mog, C, stats, centroids = cv2.connectedComponentsWithStats(I);
-    for i in range(1,n_mog):
-        if stats[i][4] < 60:
-            I[C == i] = 0
+    # n_mog, C, stats, centroids = cv2.connectedComponentsWithStats(I);
+    # for i in range(1,n_mog):
+    #     if stats[i][4] < 60:
+    #         I[C == i] = 0
 
-    kernel = np.ones((9, 9), np.uint8)
+    kernel = np.ones((7, 7), np.uint8)
     I = cv2.morphologyEx(I, cv2.MORPH_CLOSE, kernel)
 
     _, contours, _ = cv2.findContours(I, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
         x, y, w, h = cv2.boundingRect(cnt)
-        if area > 40:
+        if area > 800 * y/960:
             if w >= 3*h: continue
             # if w > h:
             #     w //=3
@@ -57,8 +55,7 @@ while True:
 
 
     if ret == True:
-        cv2.imshow('soccer',I)
-        cv2.imshow('soccer mask knn',ROI)
+        cv2.imshow('soccer mask',ROI)
 
         
     else:
