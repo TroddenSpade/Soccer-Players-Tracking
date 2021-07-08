5e06c01f651a55f22e44fc475a22021357cbb409
Parsa Samadnejad <troddenspade@gmail.com>
2021-07-09 02:37:49 +0430

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,10 @@ knn_backsub = cv2.createBackgroundSubtractorKNN(detectShadows=True) #80
 # backSub = cv2.bgsegm.BackgroundSubtractorGMG() #20
 # mog_backsub = cv2.bgsegm.createBackgroundSubtractorMOG()
 
-file_name = '0_1_mini'
-cap = cv2.VideoCapture("./videos/" + file_name + ".mp4")
+cap0 = cv2.VideoCapture("./videos/" + '0_0' + ".mp4")
+cap1 = cv2.VideoCapture("./videos/" + '0_1' + ".mp4")
+cap2 = cv2.VideoCapture("./videos/" + '0_2' + ".mp4")
 
-if (cap.isOpened()== False):
-    print("Error opening video stream or file")
-
-CUT_X = 70
 H0 = get_perspective_transform(type=0)
 H1 = get_perspective_transform(type=1)
 H2 = get_perspective_transform(type=2)
@@ -29,59 +26,80 @@ names = []
 
 while True:
     frame_count += 1
-    if frame_count > 400:
-        break
-    ret, I_org = cap.read()
-    if not ret:
+    ret0, I0 = cap0.read()
+    ret1, I1 = cap1.read()
+    ret2, I2 = cap2.read()
+    if not ret1 and not ret0 and not ret2:
         break
 
     FIELD = cv2.imread("field.jpg")
     
-    binary_img = bgs(I_org)
-
-    feet, rec_bounds, bounds = get_contours(binary_img) 
+    binary_img1 = bgs(I1, type=1)
+    feet, rec_bounds, bounds, areas = get_contours(binary_img1) 
 
     player_imgs = []
     circles = []
+
+    ## Camera 0
+    # pts = np.array(feet, np.float32)
+    # if len(pts) > 0:
+    #     out1 = cv2.perspectiveTransform(np.array(pts, np.float32), H1).reshape(-1,2)
+    #     for i, pt in enumerate(out1):
+    #         # filter based on area and position
+    #         if pt[1]*pt[1]/150 < areas[i]:
+    #             x,y, z,v = bounds[i]
+
+    #             preporcc_img = preprocess(I1[y:v, x:z])
+    #             player_imgs.append(preporcc_img)
+
+    #             circles.append((pt[0], pt[1]))
+    #             cv2.rectangle(I1, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 1)
+    #             cv2.putText(I1, str(areas[i]) + " - " + str(pt[1]), (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 1)
+
+    ## Camera 1
     pts = np.array(feet, np.float32)
     if len(pts) > 0:
-        out = cv2.perspectiveTransform(np.array(pts, np.float32), H1).reshape(-1,2)
-        for i, pt in enumerate(out):
-            if pt[1] > 30:
+        out1 = cv2.perspectiveTransform(np.array(pts, np.float32), H1).reshape(-1,2)
+        for i, pt in enumerate(out1):
+            # filter based on area and position
+            if pt[1]*pt[1]/150 < areas[i]:
                 x,y, z,v = bounds[i]
 
-                preporcc_img = preprocess(I_org[y:v, x:z])
+                preporcc_img = preprocess(I1[y:v, x:z])
                 player_imgs.append(preporcc_img)
-                # print("/////////////")
-                # np.save("p.npy", preporcc_img)
-                # cv2.imshow("x", preporcc_img)
-                # cv2.waitKey()
 
                 circles.append((pt[0], pt[1]))
-                cv2.rectangle(I_org, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 2)
+                # cv2.rectangle(I1, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 1)
+                # cv2.putText(I1, strq(areas[i]) + " - " + str(pt[1]), (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 1)
 
-                ######################
-                ###################### box saving
-                # if frame_count % 15 == 0 & True: #frame_count > 1000;
-                #     name = "img/" + str(file_name) + "-" + str(frame_count) + "-" + str(img_count) + '.jpg'
-                #     cv2.imwrite(name, I_org[y:v, x:z])
+                ##################### box saving
+                # if frame_count % 15 == 0:
+                #     name = "img/" + "0_0" + "-" + str(frame_count) + "-" + str(img_count) + '.jpg'
+                #     cv2.imwrite(name, I1[y:v, x:z])
                 #     names.append(name)
                 #     img_count += 1
 
     if len(player_imgs) > 0:
         res = classify(np.array(player_imgs))
 
-    for i, pts in enumerate(circles):
+    for i, point in enumerate(circles):
         if res[i] == 0:
-            cv2.circle(FIELD, pts, 5, (0, 0, 255), -1) # center
+            cv2.circle(FIELD, point, 7, (255, 0, 0), -1)
         elif res[i] == 1:
-            cv2.circle(FIELD, pts, 5, (0, 255, 255), -1) # center
+            cv2.circle(FIELD, point, 7, (0, 255, 255), -1)
         elif res[i] == 2:
-            cv2.circle(FIELD, pts, 5, (255, 0, 0), -1) # center
+            cv2.circle(FIELD, point, 7, (0, 0, 255), -1)
 
 
-    if ret == True:
-        cv2.imshow('soccer mask', I_org)
+    scale = 0.4
+    width = int(I1.shape[1] * scale)
+    height = int(I1.shape[0] * scale)
+    dim = (width, height)
+    
+    I = np.hstack((cv2.resize(I0, dim), cv2.resize(I1, dim), cv2.resize(I2, dim)))
+
+    if ret1 == True and ret0 == True and ret2 == True:
+        cv2.imshow('soccer mask', I)
         cv2.imshow('soccer Map', FIELD)
     else:
         break
@@ -93,5 +111,7 @@ while True:
 # names_arr = np.array(names)
 # np.save("names.npy", names_arr)
 
-cap.release()
+cap0.release()
+cap1.release()
+cap2.release()
 cv2.destroyAllWindows()
