8d66e63d6811a7fb7e6fd2a0ebf9610801727cb9
Parsa Samadnejad <troddenspade@gmail.com>
2021-07-09 02:36:20 +0430

    Update contour.py

--- src/contour.py
+++ src/contour.py
@@ -5,11 +5,12 @@ def get_contours(bin_image):
     feet = []
     rec_bounds = []
     bounds = []
-    _, contours, _ = cv2.findContours(bin_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    areas = []
+    contours, _ = cv2.findContours(bin_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
-        area = cv2.contourArea(cnt)
         x, y, w, h = cv2.boundingRect(cnt)
-        if (area > 50) and (w<2.3*h):            
+        area = cv2.contourArea(cnt)
+        if (area > 70) and (w<2.3*h):            
             M = cv2.moments(cnt)
             cX = int(M["m10"] / M["m00"])
 
@@ -20,5 +21,6 @@ def get_contours(bin_image):
             feet.append([[x_p, y+h]])
             rec_bounds.append([(cX-l,y),(cX+l,y+h)])
             bounds.append((x,y,x+w,y+h))
+            areas.append(area)
 
-    return feet, rec_bounds, bounds
+    return feet, rec_bounds, bounds, areas
