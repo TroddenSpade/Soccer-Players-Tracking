b1cc9edd72c9c03e7bc498511cf5a5a01b2ed5a1
Parsa Samadnejad <troddenspade@gmail.com>
2021-07-09 23:27:44 +0430

    Update classifier.py

--- src/classifier.py
+++ src/classifier.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 import cv2
 
-model = tf.keras.models.load_model('models/spc1.h5')
+model = tf.keras.models.load_model('models/spc3.h5')
 
 def classify(X):
     res = model.predict(X)
@@ -12,4 +12,7 @@ def classify(X):
 def preprocess(img):
     img = img.astype(np.float32) / 255
     resized_image = cv2.resize(img, (10, 28))
+    # means = resized_image.mean(axis=(0,1), dtype='float64')
+    # std = resized_image.std(axis=(0,1), dtype='float64')
+    # normalized_img = (resized_image - means) / std
     return resized_image
