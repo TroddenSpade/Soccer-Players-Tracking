fb9489f9c5c00ba820c4d6d993d871fbdf051028
Parsa Samadnejad <troddenspade@gmail.com>
2021-07-09 23:24:21 +0430

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from src.contour import get_contours
 from src.classifier import classify, preprocess
 
 
-cap0 = cv2.VideoCapture("./videos/" + '0_0' + ".mp4")
-cap1 = cv2.VideoCapture("./videos/" + '0_1' + ".mp4")
-cap2 = cv2.VideoCapture("./videos/" + '0_2' + ".mp4")
+cap0 = cv2.VideoCapture("./videos/" + '1_0' + ".mp4")
+cap1 = cv2.VideoCapture("./videos/" + '1_1' + ".mp4")
+cap2 = cv2.VideoCapture("./videos/" + '1_2' + ".mp4")
 
 H0 = get_perspective_transform(type=0)
 H1 = get_perspective_transform(type=1)
@@ -36,6 +36,21 @@ while True:
     player_imgs = []
     circles = []
 
+    # output_size = (FIELD.shape[1], FIELD.shape[0])
+    # J0 = cv2.warpPerspective(I0, H0, output_size)
+    # J1 = cv2.warpPerspective(I1, H1, output_size)
+    # J2 = cv2.warpPerspective(I2, H2, output_size)
+    # mask0 = np.load("masks/I_left.npy").astype(np.uint8)
+    # mask1 = np.load("masks/I_center.npy").astype(np.uint8)
+    # mask2 = np.load("masks/I_right.npy").astype(np.uint8)
+    # ROI0 = cv2.bitwise_and(J0, J0, mask = mask0)
+    # ROI1 = cv2.bitwise_and(J1, J1, mask = mask1)
+    # ROI2 = cv2.bitwise_and(J2, J2, mask = mask2)
+    # scale = 0.7
+    # width = int(FIELD.shape[1] * scale)
+    # height = int(FIELD.shape[0] * scale)
+    # dim = (width, height)
+
     
     binary_img0 = bgs(I0, type=0)
     feet, rec_bounds, bounds, areas = get_contours(binary_img0) 
@@ -45,13 +60,15 @@ while True:
         out1 = cv2.perspectiveTransform(np.array(pts, np.float32), H0).reshape(-1,2)
         for i, pt in enumerate(out1):
             # filter based on area and position
-            if left_mask[int(pt[1]), int(pt[0])]: # and pt[1]*pt[1]/150 < areas[i]:
+            if left_mask[int(pt[1]), int(pt[0])] and pt[1]**2/300 < areas[i]:
                 x,y, z,v = bounds[i]
 
                 preporcc_img = preprocess(I0[y:v, x:z])
                 player_imgs.append(preporcc_img)
                 circles.append((pt[0], pt[1]))
                 cv2.rectangle(I0, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 1)
+                # cv2.circle(ROI0, (pt[0], pt[1]), 5, (0, 255, 255), -1)
+
 
     binary_img1 = bgs(I1, type=1)
     feet, rec_bounds, bounds, areas = get_contours(binary_img1) 
@@ -61,29 +78,33 @@ while True:
         out1 = cv2.perspectiveTransform(np.array(pts, np.float32), H1).reshape(-1,2)
         for i, pt in enumerate(out1):
             # filter based on area and position
-            if center_mask[int(pt[1]), int(pt[0])] and pt[1]*pt[1]/170 < areas[i]:
+            if center_mask[int(pt[1]), int(pt[0])] and pt[1]**2/240 < areas[i]:
                 x,y, z,v = bounds[i]
 
                 preporcc_img = preprocess(I1[y:v, x:z])
                 player_imgs.append(preporcc_img)
                 circles.append((pt[0], pt[1]))
                 cv2.rectangle(I1, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 1)
+                # cv2.circle(ROI1, (pt[0], pt[1]), 5, (0, 255, 255), -1)
+
 
     binary_img2 = bgs(I2, type=2)
     feet, rec_bounds, bounds, areas = get_contours(binary_img2) 
-    ## Camera 0
+    ## Camera 2
     pts = np.array(feet, np.float32)
     if len(pts) > 0:
         out1 = cv2.perspectiveTransform(np.array(pts, np.float32), H2).reshape(-1,2)
         for i, pt in enumerate(out1):
             # filter based on area and position
-            if right_mask[int(pt[1]), int(pt[0])]: # and pt[1]*pt[1]/150 < areas[i]:
-                x,y, z,v = bounds[i]
+            x,y, z,v = bounds[i]
+            if right_mask[int(pt[1]), int(pt[0])] and I2[y:v, x:z].mean() > 37: # and pt[1]*pt[1]/150 < areas[i]:
 
                 preporcc_img = preprocess(I2[y:v, x:z])
                 player_imgs.append(preporcc_img)
                 circles.append((pt[0], pt[1]))
                 cv2.rectangle(I2, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 1)
+                # cv2.circle(ROI2, (pt[0], pt[1]), 5, (0, 255, 255), -1)
+                # cv2.putText(I2, str(I2[y:v, x:z].mean()), (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
 
     if len(player_imgs) > 0:
         res = classify(np.array(player_imgs))
@@ -103,6 +124,11 @@ while True:
     dim = (width, height)
     
     I = np.hstack((cv2.resize(I0, dim), cv2.resize(I1, dim), cv2.resize(I2, dim)))
+
+
+    # J = np.hstack((cv2.resize(ROI0, dim), cv2.resize(ROI1, dim), cv2.resize(ROI2, dim)))
+    # cv2.imshow('J2', J)
+
 
     if ret1 == True and ret0 == True and ret2 == True:
         cv2.imshow('soccer mask', I)
