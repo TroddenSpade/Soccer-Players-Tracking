d1cadf5b88aeee22e1e480fbc83ea1438ed36c7a
Parsa Samadnejad <troddenspade@gmail.com>
2021-07-11 00:56:59 +0430

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from src.contour import get_contours
 from src.classifier import classify, preprocess
 
 
+SKIP_FRAME = 10
+TRACKING = False
+
 cap0 = cv2.VideoCapture("./videos/" + '1_0' + ".mp4")
 cap1 = cv2.VideoCapture("./videos/" + '1_1' + ".mp4")
 cap2 = cv2.VideoCapture("./videos/" + '1_2' + ".mp4")
@@ -26,7 +29,6 @@ trackers0 = cv2.legacy.MultiTracker_create()
 trackers1 = cv2.legacy.MultiTracker_create()
 trackers2 = cv2.legacy.MultiTracker_create()
 
-SKIP_FRAME = 10
 frame_count = 0
 img_count = 0
 names = []
@@ -44,26 +46,26 @@ while True:
     player_imgs = []
     circles = []
 
-    # output_size = (FIELD.shape[1], FIELD.shape[0])
-    # J0 = cv2.warpPerspective(I0, H0, output_size)
-    # J1 = cv2.warpPerspective(I1, H1, output_size)
-    # J2 = cv2.warpPerspective(I2, H2, output_size)
-    # mask0 = np.load("masks/I_left.npy").astype(np.uint8)
-    # mask1 = np.load("masks/I_center.npy").astype(np.uint8)
-    # mask2 = np.load("masks/I_right.npy").astype(np.uint8)
-    # ROI0 = cv2.bitwise_and(J0, J0, mask = mask0)
-    # ROI1 = cv2.bitwise_and(J1, J1, mask = mask1)
-    # ROI2 = cv2.bitwise_and(J2, J2, mask = mask2)
-    # scale = 0.7
-    # width = int(FIELD.shape[1] * scale)
-    # height = int(FIELD.shape[0] * scale)
-    # dim = (width, height)
+    output_size = (FIELD.shape[1], FIELD.shape[0])
+    J0 = cv2.warpPerspective(I0, H0, output_size)
+    J1 = cv2.warpPerspective(I1, H1, output_size)
+    J2 = cv2.warpPerspective(I2, H2, output_size)
+    mask0 = np.load("masks/I_left.npy").astype(np.uint8)
+    mask1 = np.load("masks/I_center.npy").astype(np.uint8)
+    mask2 = np.load("masks/I_right.npy").astype(np.uint8)
+    ROI0 = cv2.bitwise_and(J0, J0, mask = mask0)
+    ROI1 = cv2.bitwise_and(J1, J1, mask = mask1)
+    ROI2 = cv2.bitwise_and(J2, J2, mask = mask2)
+    scale = 0.7
+    width = int(FIELD.shape[1] * scale)
+    height = int(FIELD.shape[0] * scale)
+    dim = (width, height)
 
     # ## Camera 0
     binary_img0 = bgs(I0, type=0)
-    (success, boxes) = trackers0.update(I0)
-    if frame_count % SKIP_FRAME == 0: 
-        trackers0 = cv2.legacy.MultiTracker_create()
+    if TRACKING: (success, boxes) = trackers0.update(I0)
+    if frame_count % SKIP_FRAME == 0 or not TRACKING: 
+        if TRACKING: trackers0 = cv2.legacy.MultiTracker_create()
         feet, rec_bounds, boxes, areas = get_contours(binary_img0)
         pts = np.array(feet, np.float32)
         if len(pts) > 0:
@@ -71,13 +73,14 @@ while True:
             for i, pt in enumerate(out0):
                 if left_mask[int(pt[1]), int(pt[0])] and pt[1]**2/300 < areas[i]:
                     x, y, w, h = boxes[i]
-                    tracker = tracker_method()
-                    trackers0.add(tracker, I0, boxes[i])
+                    if TRACKING:
+                        tracker = tracker_method()
+                        trackers0.add(tracker, I0, boxes[i])
                     preporcc_img = preprocess(I0[y:y+h, x:x+w])
                     player_imgs.append(preporcc_img)
                     circles.append((pt[0], pt[1]))
-                    # cv2.rectangle(I1, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 1)
-                    # cv2.circle(ROI1, (pt[0], pt[1]), 5, (0, 255, 255), -1)
+                    cv2.rectangle(I0, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 1)
+                    cv2.circle(ROI0, (int(pt[0]), int(pt[1])), 5, (0, 255, 255), -1)
     else:
         feet = []
         for box in boxes:
@@ -93,22 +96,23 @@ while True:
     # ## Camera 1
     binary_img1 = bgs(I1, type=1)
     (success, boxes) = trackers1.update(I1)
-    if frame_count % SKIP_FRAME == 0: 
-        trackers1 = cv2.legacy.MultiTracker_create()
+    if frame_count % SKIP_FRAME == 0 or not TRACKING: 
+        if TRACKING: trackers1 = cv2.legacy.MultiTracker_create()
         feet, rec_bounds, boxes, areas = get_contours(binary_img1) 
         pts = np.array(feet, np.float32)
         if len(pts) > 0:
             out1 = cv2.perspectiveTransform(np.array(pts, np.float32), H1).reshape(-1,2)
             for i, pt in enumerate(out1):
-                if center_mask[int(pt[1]), int(pt[0])] and pt[1]**2/200 < areas[i]:
+                if center_mask[int(pt[1]), int(pt[0])] and pt[1]**2/240 < areas[i]:
                     x, y, w, h = boxes[i]
-                    tracker = tracker_method()
-                    trackers1.add(tracker, I1, boxes[i])
+                    if TRACKING:
+                        tracker = tracker_method()
+                        trackers1.add(tracker, I1, boxes[i])
                     preporcc_img = preprocess(I1[y:y+h, x:x+w])
                     player_imgs.append(preporcc_img)
                     circles.append((pt[0], pt[1]))
-                    # cv2.rectangle(I1, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 1)
-                    # cv2.circle(ROI1, (pt[0], pt[1]), 5, (0, 255, 255), -1)
+                    cv2.rectangle(I1, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 1)
+                    cv2.circle(ROI1, (int(pt[0]), int(pt[1])), 5, (0, 255, 255), -1)
     else:
         feet = []
         for box in boxes:
@@ -125,8 +129,8 @@ while True:
     # ## Camera 2
     binary_img2 = bgs(I2, type=2)
     (success, boxes) = trackers2.update(I2)
-    if frame_count % SKIP_FRAME == 0: 
-        trackers2 = cv2.legacy.MultiTracker_create()
+    if frame_count % SKIP_FRAME == 0 or not TRACKING: 
+        if TRACKING: trackers2 = cv2.legacy.MultiTracker_create()
         feet, rec_bounds, boxes, areas = get_contours(binary_img2) 
         pts = np.array(feet, np.float32)
         if len(pts) > 0:
@@ -134,13 +138,14 @@ while True:
             for i, pt in enumerate(out2):
                 if right_mask[int(pt[1]), int(pt[0])] and I2[y:y+h, x:x+w].mean() > 37: # and pt[1]*pt[1]/150 < areas[i]:                    
                     x, y, w, h = boxes[i]
-                    tracker = tracker_method()
-                    trackers2.add(tracker, I2, boxes[i])
+                    if TRACKING:
+                        tracker = tracker_method()
+                        trackers2.add(tracker, I2, boxes[i])
                     preporcc_img = preprocess(I2[y:y+h, x:x+w])
                     player_imgs.append(preporcc_img)
                     circles.append((pt[0], pt[1]))
-                    # cv2.rectangle(I1, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 1)
-                    # cv2.circle(ROI1, (pt[0], pt[1]), 5, (0, 255, 255), -1)
+                    cv2.rectangle(I2, rec_bounds[i][0], rec_bounds[i][1], (0,0,255), 1)
+                    cv2.circle(ROI2, (int(pt[0]), int(pt[1])), 5, (0, 255, 255), -1)
     else:
         feet = []
         for box in boxes:
@@ -153,7 +158,7 @@ while True:
             for i, pt in enumerate(out1):
                 circles.append((pt[0], pt[1]))
 
-    if frame_count % SKIP_FRAME == 0 and len(player_imgs) > 0:
+    if (not TRACKING or frame_count % SKIP_FRAME == 0) and len(player_imgs) > 0:
         res = classify(np.array(player_imgs))
 
     for i, point in enumerate(circles):
@@ -165,16 +170,16 @@ while True:
             cv2.circle(FIELD, (int(point[0]), int(point[1])), 7, (0, 0, 255), -1)
 
 
-    scale = 0.4
-    width = int(I1.shape[1] * scale)
-    height = int(I1.shape[0] * scale)
+    scale = 1
+    width = int(FIELD.shape[1] * scale)
+    height = int(FIELD.shape[0] * scale)
     dim = (width, height)
     
     I = np.hstack((cv2.resize(I0, dim), cv2.resize(I1, dim), cv2.resize(I2, dim)))
 
 
-    # J = np.hstack((cv2.resize(ROI0, dim), cv2.resize(ROI1, dim), cv2.resize(ROI2, dim)))
-    # cv2.imshow('J2', J)
+    J = np.hstack((cv2.resize(ROI0, dim), cv2.resize(ROI1, dim), cv2.resize(ROI2, dim)))
+    cv2.imshow('J2', J)
 
 
     cv2.imshow('soccer mask', I)
